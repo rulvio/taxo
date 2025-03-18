c2e0c71142e9e68b32bfdcd6efe6246c0e6b556e
Jose Gomez <k13gomez@gmail.com>
2025-03-17 23:22:26 -0500

    chore: add missing docs dep

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Taxo.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}]
   end
 
   defp description() do
